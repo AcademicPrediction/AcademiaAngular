689084184b492a6d74b1bab28f765f9de66163e0
ThePain99 <u201823838@upc.edu.pe>
2023-08-28 16:02:09 -0500

    Fix for doPrediction

--- src/app/pages/supervisor/prediction-page/prediction-page.component.ts
+++ src/app/pages/supervisor/prediction-page/prediction-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Supervisor } from 'src/app/model/supervisor';
 import { PredictionService } from 'src/app/service/prediction.service';
 
 @Component({
@@ -16,6 +17,7 @@ export class PredictionPageComponent {
   file: any;
   isLoading: boolean = false;
   showDialog = false;
+  supervisor: Supervisor | null = null;
 
   ngOnInit(): void {
     this.file = null;
@@ -43,31 +45,37 @@ export class PredictionPageComponent {
 
   doPrediction(): void {
     this.isLoading = true;
-    this.predictionService.doPrediction(this.file, 1).subscribe(
-      (response) => {
-        const filename =
-          response.headers.get('content-disposition')?.split('filename=')[1] ||
-          'default-filename.xlsx';
-        const blob = new Blob([response.body as Blob], {
-          type: 'application/octet-stream',
-        });
-        const url = window.URL.createObjectURL(blob);
+    this.supervisor = JSON.parse(
+      localStorage.getItem('supervisor') || '{}',
+    ) as Supervisor;
+    this.predictionService
+      .doPrediction(this.file, this.supervisor.id)
+      .subscribe(
+        (response) => {
+          const filename =
+            response.headers
+              .get('content-disposition')
+              ?.split('filename=')[1] || 'default-filename.xlsx';
+          const blob = new Blob([response.body as Blob], {
+            type: 'application/octet-stream',
+          });
+          const url = window.URL.createObjectURL(blob);
 
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = filename;
-        link.click();
+          const link = document.createElement('a');
+          link.href = url;
+          link.download = filename;
+          link.click();
 
-        window.URL.revokeObjectURL(url);
+          window.URL.revokeObjectURL(url);
 
-        this.isLoading = false;
-        this.showDialog = true;
-      },
-      (error) => {
-        console.error('Error al descargar el archivo:', error);
-        this.isLoading = false;
-      },
-    );
+          this.isLoading = false;
+          this.showDialog = true;
+        },
+        (error) => {
+          console.error('Error al descargar el archivo:', error);
+          this.isLoading = false;
+        },
+      );
   }
 
   cancelPrediction(): void {
