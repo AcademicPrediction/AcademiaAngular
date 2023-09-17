7607697747b541768d7f2a93f66d38f90e6bcc75
ThePain99 <u201823838@upc.edu.pe>
2023-09-16 23:20:31 -0500

    Add new html services fields for email

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -6,65 +6,69 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
   phoneInvalid = false;
   emailSent = false;
   errorMessage = '';
 
-  constructor(private emailService: EmailService, private router: Router) { }
+  constructor(
+    private emailService: EmailService,
+    private router: Router,
+  ) {}
 
   handleSubmit(f: NgForm) {
     const phoneValue = String(f.value.InputCno);
     this.phoneInvalid = phoneValue.length !== 9;
 
-    console.log("Datos del formulario:", {
+    console.log('Datos del formulario:', {
       name: f.value.InputName,
       email: f.value.InputEmail,
       phone: phoneValue,
-      message: f.value.InputMessage
+      message: f.value.InputMessage,
     });
 
     if (this.phoneInvalid) {
-      console.log("Número de teléfono inválido");
+      console.log('Número de teléfono inválido');
       return;
     }
 
     if (f.valid && !this.phoneInvalid) {
-      console.log("Formulario válido, enviando datos...");
+      console.log('Formulario válido, enviando datos...');
 
       const emailDto = {
         name: String(f.value.InputName),
         email: String(f.value.InputEmail),
         phone: phoneValue,
-        message: String(f.value.InputMessage)
+        message: String(f.value.InputMessage),
+        messageType: '2',
       };
 
       this.emailService.sendEmail(emailDto).subscribe(
         (response: any) => {
-          if (response === "Email sent") {
+          if (response === 'Email sent') {
             this.emailSent = true;
-            console.log("Correo enviado exitosamente");
+            console.log('Correo enviado exitosamente');
           } else {
-            console.error("Respuesta inesperada del servidor:", response);
+            console.error('Respuesta inesperada del servidor:', response);
             this.errorMessage = 'Error inesperado del servidor';
             this.emailSent = false;
           }
         },
-        error => {
-          console.error("Error al enviar el correo:", error);
+        (error) => {
+          console.error('Error al enviar el correo:', error);
           this.errorMessage = 'Error al enviar el correo';
           this.emailSent = false;
-        }
+        },
       );
     } else {
-      console.log("Formulario inválido, revise los campos.");
+      console.log('Formulario inválido, revise los campos.');
     }
   }
 
   restrictInput(event: any) {
-    if (event.target.value.length >= 9 && event.key !== "Backspace") {
+    if (event.target.value.length >= 9 && event.key !== 'Backspace') {
       event.preventDefault();
     }
   }
